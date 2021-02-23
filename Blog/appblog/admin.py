b34f61717c5265651b40a05e5987920317f84f7a
rgoiss <uphuners@gmai.com>
2021-02-23 12:22:04 -0300

    Changes in models.py and admin.py. Criation of two more class model: User and Comments.

--- Blog/appblog/admin.py
+++ Blog/appblog/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from appblog.models import Story, Author
+from appblog.models import Author, Comments, Story
 
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description')
+    list_display = ('name', 'bio')
+
+
+@admin.register(Comments)
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ('comment', 'published_in', 'story')
 
 
 @admin.register(Story)
--- Blog/appblog/models.py
+++ Blog/appblog/models.py
@@ -2,12 +2,17 @@ import uuid
 from django.db import models
 
 
-class Author(models.Model):
+class User(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
     name = models.CharField(max_length=20, help_text='Author name')
-    description = models.CharField(
-        max_length=200, help_text='Say something about you'
-    )
+    bio = models.TextField()
 
     def __str__(self):
         return self.name
@@ -21,5 +26,27 @@ class Story(models.Model):
         'Author', on_delete=models.CASCADE, blank=True, null=True
     )
 
+    class Meta:
+
+        ordering = ['-pubdate']
+
     def __str__(self):
         return self.title
+
+
+class Comments(models.Model):
+    user = models.ForeignKey(
+        'User', on_delete=models.CASCADE, blank=True, null=True
+    )
+    story = models.ForeignKey(
+        'Story', on_delete=models.CASCADE, blank=True, null=True
+    )
+    comment = models.TextField()
+    published_in = models.DateTimeField(auto_now=True, editable=False)
+
+    class Meta:
+
+        ordering = ['-published_in']
+
+    def __str__(self):
+        return self.comment
