06fafcd4e7230cea92d7aa1ea9e708ceae15f118
rgoiss <uphuners@gmai.com>
2021-02-26 23:40:58 -0300

    Adicionado o model User Criação da view index, do template base e do template index.

--- Blog/appblog/admin.py
+++ Blog/appblog/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from appblog.models import Author, Comments, Story
+from appblog.models import User, Author, Story, Comments
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id')
 
 
 @admin.register(Author)
@@ -7,11 +12,11 @@ class AuthorAdmin(admin.ModelAdmin):
     list_display = ('name', 'bio')
 
 
-@admin.register(Comments)
-class CommentsAdmin(admin.ModelAdmin):
-    list_display = ('comment', 'published_in', 'story')
-
-
 @admin.register(Story)
 class StoryAdmin(admin.ModelAdmin):
     list_display = ('title', 'pubdate', 'author')
+
+
+@admin.register(Comments)
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ('comment', 'published_in', 'story')
--- Blog/appblog/views.py
+++ Blog/appblog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from appblog.models import Author, Story
 
-# Create your views here.
+
+def index(request):
+    """View for the home page."""
+
+    num_stories = Story.objects.all().count()
+    num_authors = Author.objects.all().count()
+
+    context = {
+        'num_stories': num_stories,
+        'num_authors': num_authors,
+    }
+
+    return render(request, 'index.html', context=context)
